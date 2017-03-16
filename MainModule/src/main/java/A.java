98a2244ae5c5ae0cbd2c828094413b57e72b736a
admin <isiman@yandex.ru>
2017-03-16 18:29:43 +0300

    My first commit

--- MainModule/src/main/java/A.java
+++ MainModule/src/main/java/A.java
@@ -1,8 +1,11 @@
 /**
  * Created by 6 on 16.03.2017.
  */
+
+import javascjool.utils.B;
 public class A {
     public static void main(String[] args) {
+        new B().out();
         System.out.println("Hello world!");
     }
 
---
+++ MainModule/src/main/java/javascjool/utils/B.java
@@ -0,0 +1,10 @@
+package javascjool.utils;
+
+/**
+ * Created by 6 on 16.03.2017.
+ */
+public class B {
+     public static void out() {
+          System.out.println("Im b");
+     }
+}
