6eb96a956d970705973ecc0402ee3e2d7cb3843d
admin <isiman@yandex.ru>
2017-04-05 17:49:32 +0300

    My first commit

---
+++ MainModule/src/main/java/Java_collection.java
@@ -0,0 +1,15 @@
+/**
+ * Created by 6 on 23.03.2017.
+ */
+public class Java_collection {
+    public static void main(String[] args){
+        String[][] cats ={{"Tom","1"},{"Barsik","2"},{"Mursik","3"}};
+        for(int i = 0; i<cats.length; i++)
+            for(int j = 0; j<cats[i].length; j++){
+                //System.out.print(cats[i][j] + " ");
+                System.out.println("СИМАН ЯНИС, где ваша домашняя работа?");
+            }
+
+        //System.out.println("#@");
+    }
+}
